7daeccd432f23eec10dff1753faf0ec1a9f76741
Ilya Shaisultanov <ishaisultanov@shutterstock.com>
2016-01-18 09:55:26 -0800

    WIP on the detail view 🚧

--- pokedex/PokemonDetailVC.swift
+++ pokedex/PokemonDetailVC.swift
@@ -12,6 +12,17 @@ class PokemonDetailVC: UIViewController {
     var pokemon: Pokemon!
     
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var defenceLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var pokedexLabel: UILabel!
+    @IBOutlet weak var attackLabel: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,5 +30,9 @@ class PokemonDetailVC: UIViewController {
     
     override func viewWillAppear(animated: Bool) {
         label.text = pokemon.name
+    }
+    
+    @IBAction func onBackPressed(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
 }
